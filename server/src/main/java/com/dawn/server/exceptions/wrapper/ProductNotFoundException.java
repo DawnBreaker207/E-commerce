6ab6309a3ef37c03d7c42e3fb13429dbe876e306
DawnBreaker <tunganhngo207@gmail.com>
2025-05-20 20:31:38 +0700

    feat: update exceptions

---
+++ server/src/main/java/com/dawn/server/exceptions/wrapper/ProductNotFoundException.java
@@ -0,0 +1,25 @@
+package com.dawn.server.exceptions.wrapper;
+
+import java.io.Serial;
+
+public class ProductNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ProductNotFoundException() {
+	super();
+    }
+
+    public ProductNotFoundException(String message, Throwable cause) {
+	super(message, cause);
+    }
+
+    public ProductNotFoundException(String message) {
+	super(message);
+    }
+
+    public ProductNotFoundException(Throwable cause) {
+	super(cause);
+    }
+
+}
