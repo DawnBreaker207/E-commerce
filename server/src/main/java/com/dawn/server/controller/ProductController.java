d65712f520dfa4832a5cb66b0a8af4b0c9eeed58
DawnBreaker <tunganhngo207@gmail.com>
2025-05-26 22:14:46 +0700

    fix: update product controller

--- server/src/main/java/com/dawn/server/controller/ProductController.java
+++ server/src/main/java/com/dawn/server/controller/ProductController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dawn.server.dto.ProductDto;
 import com.dawn.server.service.ProductService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
