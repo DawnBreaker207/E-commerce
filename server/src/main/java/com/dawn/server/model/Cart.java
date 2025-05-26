f78d417e67e0ce1188df62604d1dea53c84f5bcf
DawnBreaker <tunganhngo207@gmail.com>
2025-05-26 15:23:01 +0700

    feat: update cart model

---
+++ server/src/main/java/com/dawn/server/model/Cart.java
@@ -0,0 +1,40 @@
+package com.dawn.server.model;
+
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "carts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id", unique = true, nullable = false, updatable = false)
+    private Integer cartId;
+    
+    
+    @Column(name = "user_id")
+    private Long userId;
+    
+    @JsonIgnore
+    @OneToMany(mappedBy = "cart" , fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<Order> orders;
+}
