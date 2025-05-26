ecaeb9576ca974b3e650cc7957a2850d292ee523
DawnBreaker <tunganhngo207@gmail.com>
2025-05-26 22:11:37 +0700

    feat: update select component

---
+++ client/src/app/shared/components/select/select.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-select',
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule, ReactiveFormsModule],
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.css',
+})
+export class SelectComponent implements ControlValueAccessor {
+  @Input() options: any[] = [];
+  @Input() optionLabel: string = 'label';
+  @Input() optionValue: string = 'value';
+  @Input() placeholder = '';
+
+  disabled = false;
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  writeValue(val: any): void {}
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl;
+  }
+}
