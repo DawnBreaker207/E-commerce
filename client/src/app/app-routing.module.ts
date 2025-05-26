667bf610ed24a43454c85d32c59d0fe428657313
DawnBreaker <tunganhngo207@gmail.com>
2025-05-26 22:13:25 +0700

    feat: update routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
+import { AdminGuard } from './core/guards/admin.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '', loadChildren: () => import('./modules/home/home.module').then((m) => m.HomeModule) },
-  { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then((m) => m.AdminModule) },
+  {
+    path: 'admin',
+    canActivate: [AdminGuard],
+    loadChildren: () => import('./modules/admin/admin.module').then((m) => m.AdminModule),
+  },
   { path: 'errors', loadChildren: () => import('./modules/error/error.module').then((m) => m.ErrorModule) },
   { path: '**', redirectTo: 'errors/404' },
 ];
