d2d68e2b4cab57cf8a69728f46e40a24b3b9c9ce
DawnBreaker <tunganhngo207@gmail.com>
2025-06-15 14:12:44 +0700

    feat: update interceptor

--- client/src/app/core/interceptor/auth.interceptor.ts
+++ client/src/app/core/interceptor/auth.interceptor.ts
@@ -2,22 +2,28 @@ import { ToastService } from '@/app/shared/services/toast.service';
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { catchError, switchMap, tap, throwError, timer } from 'rxjs';
+import { catchError, switchMap, take, tap, throwError, timer } from 'rxjs';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
   const oidc = inject(OidcSecurityService);
   const toast = inject(ToastService);
-  return next(req).pipe(
-    catchError((err) => {
-      if (err.status === 401) {
-        toast.show('Your session has expired', 'error');
-        return timer(5000).pipe(
-          tap(() => oidc.authorize()),
-          switchMap(() => throwError(() => err)),
-        );
-      }
 
-      return throwError(() => err);
-    }),
+  const isAdminApi = req.url.includes('/admin');
+  return next(req).pipe(
+    catchError((err) =>
+      oidc.isAuthenticated$.pipe(
+        take(1),
+        switchMap((isAuthenticated) => {
+          if (err.status === 401 && isAuthenticated && isAdminApi) {
+            toast.show('Your session has expired', 'error');
+            return timer(3000).pipe(
+              tap(() => oidc.authorize()),
+              switchMap(() => throwError(() => err)),
+            );
+          }
+          return throwError(() => err);
+        }),
+      ),
+    ),
   );
 };
