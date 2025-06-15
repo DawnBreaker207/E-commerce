39bc9a44bfc5b4e4738d619e48f4258f7bb926c5
DawnBreaker <tunganhngo207@gmail.com>
2025-06-15 14:03:40 +0700

    feat: update config server

--- server/src/main/java/com/dawn/server/ServerApplication.java
+++ server/src/main/java/com/dawn/server/ServerApplication.java
@@ -2,8 +2,12 @@ package com.dawn.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableCaching
+@EnableJpaAuditing
 public class ServerApplication {
 
 	public static void main(String[] args) {
