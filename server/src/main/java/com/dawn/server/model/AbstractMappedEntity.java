81f8482c260e31b7884b06375c137ca5bccdff96
DawnBreaker <tunganhngo207@gmail.com>
2025-06-15 14:08:57 +0700

    feat: update abstract mapping

--- server/src/main/java/com/dawn/server/model/AbstractMappedEntity.java
+++ server/src/main/java/com/dawn/server/model/AbstractMappedEntity.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -20,16 +23,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public abstract class AbstractMappedEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     @CreatedDate
     @JsonFormat(shape = Shape.STRING)
-    @Column(name = "created_at")
+    @Column(name = "created_at",updatable = false)
     private Instant createdAt;
 
-    @CreatedDate
+    @LastModifiedDate
     @JsonFormat(shape = Shape.STRING)
     @Column(name = "updated_at")
     private Instant updatedAt;
